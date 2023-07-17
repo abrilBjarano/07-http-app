666fffc381c5951aedecc4f9fd10661fbef3b4e4
abrilxcx <abrlbjrn@gmail.com>
2023-07-16 19:58:52 -0600

    jeje2

--- src/users/store/users-store.js
+++ src/users/store/users-store.js
@@ -10,6 +10,7 @@ const state = {
 
 const loadNextPage = async() => {
     loadUsersByPage( state.currentPage + 1 );
+    
 }
 
 const loadPreviousPage = async() => {
--- src/users/use-cases/load-users-by-page.js
+++ src/users/use-cases/load-users-by-page.js
@@ -7,7 +7,7 @@
  */
 export const loadUsersByPage = async( page = 1 ) => {
 
-    const url = `${ import.meta.env.VITE_BASE_URL }users?_page=${ page }`;
+    const url = `${ import.meta.env.VITE_BASE_URL }/users?_page=${ page }`;
     const res = await fetch(url);
     const data = await res.json();
 
