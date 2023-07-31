3423e0707e2f6b182fe2a4784a0f34b3fd677570
abrilxcx <abrlbjrn@gmail.com>
2023-07-31 14:53:29 -0600

    some icons index removed

--- src/users/users-app.js
+++ src/users/users-app.js
@@ -1,6 +1,7 @@
 import { renderButtons } from './presentation/render-buttons/render-buttons';
 import { renderTable } from './presentation/render-table/render-table';
 import usersStore from './store/users-store';
+import { loadUsersByPage } from './use-cases/load-users-by-page';
 
 /**
  * 
@@ -12,6 +13,8 @@ export const UsersApp = async( element ) => {
   await usersStore.loadNextPage();
   element.innerHTML = '';
   
+  loadUsersByPage( element );
   renderTable( element );
   renderButtons( element );
+  
 }
