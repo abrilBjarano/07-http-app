4edfb05d331d6160994b94d959254834d1653d89
abrilxcx <abrlbjrn@gmail.com>
2023-08-02 23:18:16 -0600

    175. Cargar información usuario actual

--- src/users/presentation/render-table/render-table.js
+++ src/users/presentation/render-table/render-table.js
@@ -1,4 +1,5 @@
 import usersStore from '../../store/users-store';
+import { showModal } from '../render-modal/render-modal';
 import './render-table.css';
 
 
@@ -24,6 +25,23 @@ const createTable = (  ) => {
   return table;
 }
 
+/**
+ * 
+ * @param {MouseEvent} event 
+ */
+const tableSelectListener = ( event ) => {
+  const element = event.target.closest('.select-user');
+  if( !element ) return;
+
+  const id = element.getAttribute('data-id');
+  showModal(id);
+}
+
+
+/**
+ * 
+ * @param {HTMLDivElement} element 
+ */
 export const renderTable = ( element ) => {
 
   const users = usersStore.getUsers();
@@ -33,6 +51,7 @@ export const renderTable = ( element ) => {
     element.append( table );
 
     // TODO: listeners a la tabla
+    table.addEventListener('click', tableSelectListener);
   }
 
   let tableHTML = '';
@@ -45,9 +64,9 @@ export const renderTable = ( element ) => {
         <td>${ user.lastName }</td>
         <td>${ user.isActive}</td>
         <td>
-          <a href ="#/" data-id=${ user.id } />Select</a>
+          <a href ="#/" class="select-user" data-id=${ user.id } />Select</a>
           |
-          <a href ="#/" data-id=${ user.id } />Delete</a>
+          <a href ="#/" class="delete-user" data-id=${ user.id } />Delete</a>
         </td>
       </tr>
     `
